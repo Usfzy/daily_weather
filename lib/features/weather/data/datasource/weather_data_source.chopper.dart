b7b13aee639504bab34112cead600380dabd7f78
Usfzy <usfzy47@gmail.com>
2024-03-26 21:35:33 +0000

    generated file

--- lib/features/weather/data/datasource/weather_data_source.chopper.dart
+++ lib/features/weather/data/datasource/weather_data_source.chopper.dart
@@ -16,4 +16,31 @@ final class _$WeatherDataSource extends WeatherDataSource {
 
   @override
   final Type definitionType = WeatherDataSource;
+
+  @override
+  Future<WeatherDataResponse> getWeather(
+    double latitude,
+    double longitude,
+    List<String> dailyParams,
+    int pastDays,
+    int forecastDays,
+  ) async {
+    final Uri $url = Uri.parse('/v1/forecast');
+    final Map<String, dynamic> $params = <String, dynamic>{
+      'latitude': latitude,
+      'longitude': longitude,
+      'daily': dailyParams,
+      'past_days': pastDays,
+      'forecast_days': forecastDays,
+    };
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+      parameters: $params,
+    );
+    final Response $response =
+        await client.send<WeatherDataResponse, WeatherDataResponse>($request);
+    return $response.bodyOrThrow;
+  }
 }
