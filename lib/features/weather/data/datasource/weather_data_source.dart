94396aac194a64daff53d54954a60dd55e62ee81
Usfzy <usfzy47@gmail.com>
2024-03-26 21:34:36 +0000

    weather repository and datasource implementation

--- lib/features/weather/data/datasource/weather_data_source.dart
+++ lib/features/weather/data/datasource/weather_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:chopper/chopper.dart';
+import 'package:daily_weather/features/weather/data/model/weather_data_response.dart';
 part 'weather_data_source.chopper.dart';
 
 @ChopperApi()
@@ -6,4 +7,13 @@ abstract class WeatherDataSource extends ChopperService {
   static WeatherDataSource create([ChopperClient? client]) {
     return _$WeatherDataSource(client);
   }
+
+  @Get(path: '/v1/forecast')
+  Future<WeatherDataResponse> getWeather(
+    @Query("latitude") double latitude,
+    @Query("longitude") double longitude,
+    @Query("daily") List<String> dailyParams,
+    @Query("past_days") int pastDays,
+    @Query("forecast_days") int forecastDays,
+  );
 }
--- lib/features/weather/data/repository/weather_repository.dart
+++ lib/features/weather/data/repository/weather_repository.dart
@@ -1,10 +1,24 @@
+import 'package:daily_weather/core/params/params.dart';
+import 'package:daily_weather/core/services/network_info.dart';
 import 'package:daily_weather/features/weather/data/datasource/weather_data_source.dart';
+import 'package:daily_weather/features/weather/data/model/weather_data_response.dart';
 
 class WeatherRepository {
-  const WeatherRepository({required this.dataSource});
+  const WeatherRepository({
+    required this.dataSource,
+    required this.networkInfo,
+  });
+
+  final NetworkInfo networkInfo;
   final WeatherDataSource dataSource;
 
-  Future<void> getWeather() async{
-    
+  Future<WeatherDataResponse> getWeather(WeatherParams weatherParams) async {
+    return dataSource.getWeather(
+      weatherParams.latitude,
+      weatherParams.longitude,
+      weatherParams.dailyParams,
+      weatherParams.pastDays,
+      weatherParams.forecastDays,
+    );
   }
 }
